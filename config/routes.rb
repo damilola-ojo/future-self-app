e34b8d1afa705eaa0d16a9deb76266ed8c7d7806
Damilola Ojo <tdami262@gmail.com>
2022-07-04 19:45:37 +0100

    Generate Letters Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
+  resources :letters
 
-   root "application#hello"
+  root "application#hello"
 end
