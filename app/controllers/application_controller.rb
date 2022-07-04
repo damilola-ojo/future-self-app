a59bd105f837efb2d00dc81e7f00bb40415e8383
Damilola Ojo <tdami262@gmail.com>
2022-07-04 18:54:53 +0100

    Add Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def hello
+    render html: "Welcome Onboard"
+  end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname])
+    end
 end
