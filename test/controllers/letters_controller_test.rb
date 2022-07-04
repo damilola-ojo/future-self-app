3df213f445b2ff1707ad68f3a8a568c135fb63c7
Damilola Ojo <tdami262@gmail.com>
2022-07-04 20:23:32 +0100

    Add tests for Letters Controller

--- test/controllers/letters_controller_test.rb
+++ test/controllers/letters_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class LettersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index" do
+    get letters_path
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_letter_path
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_letter_path(Letter.first)
+    assert_response :success
+  end
 end
